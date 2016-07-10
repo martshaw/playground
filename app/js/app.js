3cf2954486dc8554c4e806dce7126d51d133a61d
Martin Shaw <info@martshaw.com>
2016-07-10 23:15:19 +0200

    feat($browser): markdown-json-handlbars

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,2 @@
 import { cube } from './maths.js';
-console.log(cube(5)); // 125
+cube(5); // 125
--- gulp/tasks/templates.js
+++ gulp/tasks/templates.js
@@ -1,14 +1,27 @@
 import gulp from 'gulp';
+import tap from 'gulp-tap';
 import handlebars from 'gulp-compile-handlebars';
+import markdown from 'gulp-markdown-to-json';
 import rename from 'gulp-rename';
+import gutil from 'gulp-util';
 
-gulp.task('templates', function () {
+gulp.task('templates', ['json'], function () {
     const options = {
         batch: ['./app/templates/partials'],
     };
+    return gulp.src('./build/json/data.json')
+    .pipe(tap(function(file) {
+        const data = file.contents.toString();
+        return gulp.src('./app/templates/*.hbs')
+        .pipe(handlebars(data, options))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('./build/'));
+    }));
+});
 
-    return gulp.src('./app/templates/*.hbs')
-    .pipe(handlebars(null, options))
-    .pipe(rename('mock.html'))
-    .pipe(gulp.dest('./build/'));
+gulp.task('json', function () {
+    gulp.src('./app/assets/content/**.md')
+    .pipe(gutil.buffer())
+    .pipe(markdown('data.json'))
+    .pipe(gulp.dest('./build/json'));
 });
